84005d5357686a312b1ab5db03dd0ce7aff83412
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-11 14:11:37 +0200

    Start

---
+++ src/main/java/org/example/models/VendingMachine.java
@@ -0,0 +1,13 @@
+package org.example.models;
+
+public abstract class VendingMachine {
+
+    private int price;
+    private String name;
+    private int calories;
+    private String allergens;
+
+
+
+
+}
