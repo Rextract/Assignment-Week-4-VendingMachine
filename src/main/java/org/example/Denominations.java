a181ffab6eff02867c7a184fdb3e1c65aa86fbff
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-13 15:08:44 +0200

    Assignment VendingMachine Week 4

---
+++ src/main/java/org/example/Denominations.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public enum Denominations {
+
+    KR1(1),
+    KR2(2),
+    KR5(5),
+    KR10(10),
+    KR20(20),
+    KR50(50),
+    KR100(100),
+    KR200(200),
+    KR500(500),
+    KR1000(1000);
+
+    private final int maxValue;
+
+    private Denominations (int maxValue){
+        this.maxValue = maxValue;
+    }
+
+    public int pay(){
+        return maxValue;
+    }
+
+
+
+}
