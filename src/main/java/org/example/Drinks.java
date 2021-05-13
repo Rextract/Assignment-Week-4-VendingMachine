7e709e840a404e2473aa0570f7d4f4a466bb59e7
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-13 18:42:20 +0200

    VendingMachine Assignment

---
+++ src/main/java/org/example/Drinks.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Drinks extends Product {
+
+    private String name;
+    private int price;
+    private int calories;
+    private String allergens;
+
+    public Drinks(String name, int price, int calories, String allergens) {
+        super(name, price, calories, calories, allergens);
+        this.name = name;
+        this.price = price;
+        this.calories = calories;
+        this.allergens = allergens;
+    }
+}
--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -3,11 +3,18 @@ package org.example;
 public abstract class Product {
 
 
-    String examine(){
-        return (price, name, calories, allergens);
+    private String examine(String name, int price, int calories, String allergens) {
+        return null;
     }
-    String use(){
-        return (consume);
+
+    private String use() {
+        return null;
     }
+
+    public Product(String examine, int price, int calories, int i, String use) {
+        this.examine = examine;
+        this.use = use;
+    }
+
 
 }
