27aa0abd534c3c165080b539b22863c0a9e5841b
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-13 21:47:32 +0200

    VendingMachine Assignment

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -2,14 +2,67 @@ package org.example;
 
 public abstract class Product {
 
+    private int price;
+    private String name;
+    private int calories;
+    private String allergens;
+
+    public Product(int price, String name, int calories, String allergens) {
+        this.price = price;
+        this.name = name;
+        this.calories = calories;
+        this.allergens = allergens;
+    }
+
     public String examine(int price, String name, int calories, String allergens) {
         System.out.println(price + name + calories + allergens);
-        return 
+        return String.valueOf(0);
 
     }
 
+    public String use(){
+        return use();
+    }
 
+    public int getPrice() {
+        return price;
+    }
 
+    public void setPrice(int price) {
+        this.price = price;
+    }
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public String getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(String allergens) {
+        this.allergens = allergens;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "price=" + price +
+                ", name='" + name + '\'' +
+                ", calories=" + calories +
+                ", allergens='" + allergens + '\'' +
+                '}';
+    }
 }
--- src/main/java/org/example/VendingMachine.java
+++ src/main/java/org/example/VendingMachine.java
@@ -1,6 +1,23 @@
 package org.example;
 
+import java.util.List;
+
 public interface VendingMachine {
+
+    String REPORT_HEADER = "Report for the sales item: ";
+
+
+    static void printReports(List<VendingMachine> products){
+        for (VendingMachine product : products){
+            System.out.println(Product.class);
+        }
+    }
+
+    default String getReport(){
+        return REPORT_HEADER;
+    }
+
+
 
     void addCurrency(int amount);
 
