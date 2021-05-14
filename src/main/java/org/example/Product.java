12064ccd22f866d914833c41613398bfaaf2eb8c
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-14 17:10:00 +0200

    VendingMachine Assignment

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,19 +1,53 @@
 package org.example;
 
-public abstract class Product implements VendingMachine {
+public abstract class Product {
 
-    private int price;
-    private String name;
-    private int calories;
-    private String allergens;
+    protected int price;
+    protected String name;
+    protected int calories;
+    protected String allergens;
 
     public Product(int price, String name, int calories, String allergens) {
-        this.price = price;
-        this.name = name;
-        this.calories = calories;
-        this.allergens = allergens;
+        setPrice(price);
+        setName(name);
+        setCalories(calories);
+        setAllergens(allergens);
     }
 
+    public Product() {
 
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public String getAllergens() {
+        return allergens;
+    }
+
+    public void setAllergens(String allergens) {
+        this.allergens = allergens;
+    }
 }
 
