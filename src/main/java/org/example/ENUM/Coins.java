d94e072eeb65ca458cf3ddb1a9f7112132f23ac4
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-16 23:04:02 +0200

    VendingMachine Assignment

--- src/main/java/org/example/ENUM/Coins.java
+++ src/main/java/org/example/ENUM/Coins.java
@@ -1,4 +1,27 @@
 package org.example.ENUM;
 
 public enum Coins {
+
+    ONE_SEK (1),
+    TWO_SEK (2),
+    FIVE_SEK (5),
+    TEN_SEK (10),
+    TWENTY_SEK (20),
+    FIFTY_SEK (50),
+    ONE_HUNDRED_SEK (100),
+    TWO_HUNDRED_SEK (200),
+    FIVE_HUNDRED_SEK (500),
+    ONE_THOUSAND_SEK (1000);
+
+    private int value;
+
+            Coins (int value){
+        this.value = value;
+            }
+
+
+    public int getValue() {
+                return this.value;
+    }
+
 }
