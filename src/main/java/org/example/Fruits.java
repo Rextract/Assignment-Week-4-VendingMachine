7abd73bc5cfc7353ab2a37df4256a69beb270f34
David Ismael A.M <https://github.com/Rextract/G36_first_lecture.git>
2021-05-16 21:15:09 +0200

    VendingMachine Assignment

--- src/main/java/org/example/Fruits.java
+++ src/main/java/org/example/Fruits.java
@@ -1,10 +1,26 @@
 package org.example;
 
-public class Fruits {
+public class Fruits implements Product {
 
     private int price = 15;
     private String name = "Big Apple";
     private int calories = 125;
     private String allergens = "Has traces of Apple";
 
+    public Fruits(int price, String name, int calories, String allergens) {
+        this.price = price;
+        this.name = name;
+        this.calories = calories;
+        this.allergens = allergens;
+    }
+
+    @Override
+    public String examine() {
+        return price + name + calories + allergens;
+    }
+
+    @Override
+    public String use() {
+        return name;
+    }
 }
